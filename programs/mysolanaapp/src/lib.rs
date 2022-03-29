e41b795cf5860626e3237e1cebf7cfdba5f010af
sengwee <sengwee.samwell@gmail.com>
2022-03-29 20:08:46 +0800

    SendTweet

--- programs/mysolanaapp/src/lib.rs
+++ programs/mysolanaapp/src/lib.rs
@@ -4,17 +4,46 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod mysolanaapp {
+    use std::task::Context;
+
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
     use super::*;
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap();
+
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into())
+        }
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into())
+        }
+        tweet.author = *author.key;
+        tweet.timestamp = clock.unix_timestamp;
+        tweet.topic = topic;
+        tweet.content = content;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SendTweet<'info> {
+    #[account(init, payer = author, space = Tweet::LEN)]
+    pub tweet: Account<'info, Tweet>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    // #[account(address = system_program::ID)]
+    pub system_program: Program<'info, System>,
+}
 
 // an account that goes inside a transaction instruction
 #[account]
 pub struct Tweet {
     pub author: Pubkey,
-    pub timestamp: i65,
+    pub timestamp: i64,
     pub topic: String,
     pub content: String,
 }
@@ -36,4 +65,13 @@ impl Tweet {
         + MAX_TOPIC_LENGTH
         + STRING_LENGTH_PREFIX
         + MAX_CONTENT_LENGTH;
+}
+
+// list of error codes
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
 }
