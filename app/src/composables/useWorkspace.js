4e58cd8ee93494c582f6e46d847530e84f4d6511
sengwee <sengwee.samwell@gmail.com>
2022-03-30 22:40:42 +0800

    setting up devnet env

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -3,7 +3,11 @@ import { useAnchorWallet } from 'solana-wallets-vue'
 import { Connection, PublicKey } from '@solana/web3.js'
 import { Provider, Program } from '@project-serum/anchor'
 import idl from '../../../target/idl/mysolanaapp.json'
-
+// import idl from '@/idl/mysolanaapp.json'
+// const clusterUrl = process.env.VUE_APP_CLUSTER_URL
+const clusterUrl = "http://127.0.0.1:8899"
+const preflightCommitment = 'processed'
+const commitment = 'processed'
 const programID = new PublicKey(idl.metadata.address)
 let workspace = null
 
@@ -11,8 +15,10 @@ export const useWorkspace = () => workspace
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899')
-    const provider = computed(() => new Provider(connection, wallet.value))
+    const connection = new Connection(clusterUrl, commitment)
+    // for devnet deployment
+    // const connection = new Connection('https://api.devnet.solana.com', commitment)
+    const provider = computed(() => new Provider(connection, wallet.value, { preflightCommitment, commitment }))
     const program = computed(() => new Program(idl, programID, provider.value))
     workspace = {
         wallet,
@@ -20,5 +26,5 @@ export const initWorkspace = () => {
         provider,
         program,
     }
-    
+
 }
